a56c2287f062fa3f8c31bb96c84706ee33a0c4e9
JrzenonDev <jrobertoonb@gmail.com>
2023-06-30 00:11:33 -0300

    feat: create component Header

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+import { Header } from "./src/components/Header";
 import Home from "./src/pages/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <Header name="José Roberto" />
+      <Home />
+    </>
+  );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,54 @@
+import { Feather } from "@expo/vector-icons";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+const statusBarHeight = StatusBar.currentHeight
+  ? StatusBar.currentHeight + 22
+  : 64;
+
+export function Header({ name }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.userName}>{name}</Text>
+        <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
+          <Feather name="user" size={27} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#8000ff",
+    paddingTop: statusBarHeight,
+    flexDirection: "row",
+    paddingStart: 16,
+    paddingEnd: 16,
+    paddingBottom: 44,
+  },
+  content: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  userName: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  buttonUser: {
+    width: 44,
+    height: 44,
+    backgroundColor: "rgba(255,255,255, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 44 / 2,
+  },
+});
