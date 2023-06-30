7b916c22227771fba8a08aed9510f67b57474d36
JrzenonDev <jrobertoonb@gmail.com>
2023-06-30 01:39:50 -0300

    feat: create mock listExapnses

---
+++ src/mocks/listExpanses.js
@@ -0,0 +1,30 @@
+export const listExpanses = [
+  {
+    id: 0,
+    label: "Recebimento de salário",
+    value: "10.000,00",
+    date: "20/03/2023",
+    type: 1,
+  },
+  {
+    id: 1,
+    label: "Boleto de conta de luz",
+    value: "500,00",
+    date: "15/01/2023",
+    type: 0,
+  },
+  {
+    id: 2,
+    label: "Pix cliente Giovana",
+    value: "600,00",
+    date: "20/03/2023",
+    type: 1,
+  },
+  {
+    id: 3,
+    label: "Boleto de conta de água",
+    value: "300,00",
+    date: "16/02/2023",
+    type: 0,
+  },
+];
