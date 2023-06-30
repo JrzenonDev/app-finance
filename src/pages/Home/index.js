534029bbe0213b98437482434860eaf9c3df7bb9
JrzenonDev <jrobertoonb@gmail.com>
2023-06-30 01:41:06 -0300

    feat: listing user account movements in Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,20 @@
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { Ballance } from "../../components/Ballance";
+import { Moviments } from "../../components/Moviments";
+import { listExpanses } from "../../mocks/listExpanses";
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Ballance ballance="13.000,00" expanses="451,00" />
+      <Text style={styles.title}>Últimas movimentações</Text>
+      <FlatList
+        style={styles.list}
+        data={listExpanses}
+        keyExtractor={(item) => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <Moviments data={item} />}
+      />
     </View>
   );
 }
@@ -13,5 +23,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fafafa",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14,
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14,
   },
 });
