4b0803127fa6787daa5d56af3f9be61e015557f2
JrzenonDev <jribertoonb@gmail.com>
2023-07-06 03:29:39 -0300

    feat: adding functionality to show and hide expenses values

--- src/components/Moviments/index.js
+++ src/components/Moviments/index.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export function Moviments({ data }) {
+  const [showValue, setShowValue] = useState(false)
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={ () => setShowValue(!showValue) }>
       <Text style={styles.date}>{data.date}</Text>
       <View style={styles.content}>
         <Text style={styles.label}>{data.label}</Text>
-        <Text style={data.type === 1 ? styles.value : styles.expanses}>
+        {showValue ? (
+          <Text style={data.type === 1 ? styles.value : styles.expanses}>
           {data.type === 1 ? `R$ ${data.value}` : `R$ -${data.value}`}
         </Text>
+        ) : (
+          <View style={styles.skeleton} />
+        )}
       </View>
     </TouchableOpacity>
   );
@@ -43,4 +49,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#e74c3c",
   },
+  skeleton: {
+    marginTop: 6,
+    width: 80,
+    height: 10,
+    backgroundColor: '#dadada',
+    borderRadius: 9
+  }
 });
